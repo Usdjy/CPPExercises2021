8d3acdd25196946fcfdd2e4722f86ce4aab4bfd6
Максим Туревский <turmax20052005@gmail.com>
2022-04-25 22:21:38 +0300

    kjndnndfdnuvuvdf

--- lesson16/src/main.cpp
+++ lesson16/src/main.cpp
@@ -8,11 +8,16 @@
 #include <iostream>
 #include <filesystem>
 #include <memory>
+#include <vector>
+#include <algorithm>
+#include <set>
 
 #include <libutils/rasserts.h>
-
+using namespace std;
+using namespace cv;
 bool isPixelEmpty(cv::Vec3b color) {
     // TODO 1 реализуйте isPixelEmpty(color):
+    return !(color[0]+color[1]+color[2]);
     // - верните true если переданный цвет - полностью черный (такие пиксели мы считаем пустыми)
     // - иначе верните false
     rassert(false, "325235141242153: You should do TODO 1 - implement isPixelEmpty(color)!");
@@ -131,6 +136,65 @@ void run(std::string caseName) {
 
     cv::Mat panoDiff(pano_rows, pano_cols, CV_8UC3, cv::Scalar(0, 0, 0));
     // TODO 2 вам надо заполнить panoDiff картинку так чтобы было четко ясно где pano0 картинка (объявлена выше) и pano1 картинка отличаются сильно, а где - слабо:
+    vector<vector<int> > a(pano_rows);for(auto &a1:a) a1.resize(pano_cols);
+    for(int i=0;i<a.size();++i) for(int j=0;j<a[i].size();++j) a[i][j]=0;
+    for(int i=0;i<pano_rows;++i)
+    {
+        for(int j=0;j<pano_cols;++j)
+        {
+            cv::Vec3b color0=pano0.at<cv::Vec3b>(i,j);cv::Vec3b color1=pano1.at<cv::Vec3b>(i,j);
+            bool emp0=isPixelEmpty(color0);bool emp1=isPixelEmpty(color1);
+            if((emp0^emp1)) {panoDiff.at<cv::Vec3b>(i,j)={255,255,255};a[i][j]=1000;} else if(emp0 && emp1) {panoDiff.at<cv::Vec3b>(i,j)={0,0,0};a[i][j]=1000;}
+            else {int diff=abs(color0[0]-color1[0])+abs(color0[1]-color1[1])+abs(color0[2]-color1[2]);
+            a[i][j]=diff;
+            uchar col=std::min((uchar) 255,(uchar) diff);
+                panoDiff.at<cv::Vec3b>(i,j)={col,col,col};}
+        }
+    }
+    int n1=pano_rows;int m1=pano_cols;
+    vector<vector<bool> > used(pano_rows);vector<vector<bool> > ok(pano_rows);vector<vector<int> > is(pano_rows);vector<vector<pair<int,int> > > pr(n1);for(int i=0;i<used.size();++i) {for(int j=0;j<pano_cols;++j) {used[i].push_back(0);ok[i].push_back(0);is[i].push_back(1e9);pr[i].push_back({-1,-1});}}
+    is[n1-1][0]=0;set<pair<int,pair<int,int> > > d;for(int i=0;i<pano_rows;++i) for(int j=0;j<pano_cols;++j) d.insert({is[i][j],{i,j}});
+    while(!d.empty())
+    {
+        pair<int,pair<int,int> > o=(*d.begin());d.erase(d.begin());
+        pair<int,int> t=o.second;int x=t.first;int y=t.second;
+        int w=is[x][y]+a[x][y];
+        if(x-1>=0 && is[x-1][y]>w)
+        {
+            pr[x-1][y]={x,y};
+            d.erase({is[x-1][y],{x-1,y}});
+            is[x-1][y]=w;d.insert({is[x-1][y],{x-1,y}});
+        }
+        if(y+1<m1 && is[x][y+1]>w)
+        {
+            pr[x][y+1]={x,y};
+            d.erase({is[x][y+1],{x,y+1}});
+            is[x][y+1]=w;d.insert({is[x][y+1],{x,y+1}});
+        }
+    }
+    vector<pair<int,int> > h;pair<int,int> cur={0,m1-1};
+    while(cur.first!=(n1-1) || cur.second!=0)
+    {
+        h.push_back(cur);cur=pr[cur.first][cur.second];
+    }
+    h.push_back(cur);
+    Mat panoDiff2=panoDiff.clone();
+    for(auto h1:h)
+    {
+        ok[h1.first][h1.second]=true;
+        panoDiff2.at<cv::Vec3b>(h1.first,h1.second)={0,0,255};
+    }
+    Mat panodiff3=panoDiff.clone();
+    for(int i=0;i<n1;++i)
+    {
+        bool f=false;
+        for(int j=0;j<m1;++j)
+        {
+            f=(f || ok[i][j]);
+            if(f) {panodiff3.at<Vec3b>(i,j)=pano1.at<Vec3b>(i,j);}
+            else {panodiff3.at<Vec3b>(i,j)=pano0.at<Vec3b>(i,j);}
+        }
+    }
     // сравните в этих двух картинках пиксели по одинаковым координатам (т.е. мы сверяем картинки) и покрасьте соответствующий пиксель panoDiff по этой логике:
     // - если оба пикселя пустые - проверяйте это через isPixelEmpty(color) (т.е. цвета черные) - результат тоже пусть черный
     // - если ровно один их пикселей пустой - результат пусть идеально белый
@@ -140,6 +204,8 @@ void run(std::string caseName) {
     //  panoDiff.at<cv::Vec3b>(j, i) = cv::Vec3b(blueValue, greenValue, redValue);
 
     cv::imwrite(resultsDir + "5panoDiff.jpg", panoDiff);
+    cv::imwrite(resultsDir + "6panoDiff2.jpg", panoDiff2);
+    cv::imwrite(resultsDir + "7panoDiff3.jpg", panodiff3);
 }
 
 
@@ -148,7 +214,7 @@ int main() {
         run("1_hanging"); // TODO 3 проанализируйте результаты по фотографиям с дрона - где различие сильное, где малое? почему так?
         run("2_hiking"); // TODO 4 проанализируйте результаты по фотографиям с дрона - где различие сильное, где малое? почему так?
         run("3_aero"); // TODO 5 проанализируйте результаты по фотографиям с дрона - где различие сильное, где малое? почему так?
-        run("4_your_data"); // TODO 6 сфотографируйте что-нибудь сами при этом на второй картинке что-то изменив, проведите анализ
+        //run("4_your_data"); // TODO 6 сфотографируйте что-нибудь сами при этом на второй картинке что-то изменив, проведите анализ
         // TODO 7 проведите анализ результатов на базе Вопросов-Упражнений предложенных в последней статье "Урок 19: панорама и визуализация качества склейки"
 
         return 0;
